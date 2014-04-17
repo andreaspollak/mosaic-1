b139ce331f16c37df0d48f64c918de4399288287
pberghof <pberghof@192.168.1.30>
2014-04-17 11:09:52 +0200

    Création de la fonction dominant color

--- Mosaic.php
+++ Mosaic.php
@@ -22,6 +22,22 @@ class Mosaic {
         $file['mosaic']['file'] = $dest;
         $file['mosaic']['height'] = $size[0];
         $file['mosaic']['width'] = $size[1];
+        $file['pictures'] = array();
+        
+        $x = 0;
+        $y = 0;
+        while($x <= $this->source_size[1]) {
+            while($y <= $this->source_size[0]) {
+                $zone['x'] = $x;
+                $zone['y'] = $y;
+                $zone['colors'] = dominant_color();
+                array_push($file['pictures'], $zone);
+                $y = $y+$zone_size[0];
+            }
+            $y = 0;
+            $x = $x+$zone_size[1];
+        }
+        
         /* TODO VERIF DROIT D'ECRITURE */
         /*
         if (substr(sprintf('%o', fileperms($this->output)), -4) != 775) {
@@ -41,12 +57,48 @@ class Mosaic {
             $input_size = array($size[0],$size[1]);
             $result['intpu_size'] = $input_size;
         }
+        /*
+        $fread = json_decode(file_get_contents($this->dest_params), true);
+        
+        $fread['pictures'] = array();
+        
+        $picture['name'] = $input;
+        $picture['height'] = $size[0];
+        $picture['width'] = $size[1];
+        
+        array_push($fread['pictures'],$picture);
         
         $fopen = fopen($this->dest_params, "w+");
-        $fread = file_gets_content($this->dest_params);
-        
-        return $fread;
+        fwrite($fopen, json_encode($fread));
+        fclose($fopen);
+         * 
+         */
     }
+    
+    public function dominant_color() {
+        $i = imagecreatefromjpeg($this->source);
+        $color['rouge'] = '';
+        $color['bleu'] = '';
+        $color['gris'] = '';
+        $total = '';
+        for ($x=0;$x<imagesx($i);$x++) {
+            for ($y=0;$y<imagesy($i);$y++) {
+                $rgb = imagecolorat($i, $x, $y);
+                $r = ($rgb >> 16) & 0xFF;
+                $g = ($rgb >> 8) & 0xFF;
+                $b = $rgb & 0xFF;
+                $color['rouge'] += $r;
+                $color['gris'] += $g;
+                $color['bleu'] += $b;
+                $total++;
+            }
+        }
+        return $color;
+    }
+    
+    /* TODO */
+    /* FONCTION de place disponible */
+    /* FONCTION de nombre de participants */
 }
 
 ?>
